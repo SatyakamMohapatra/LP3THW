f2f1f150c7101d88ec6026eef3e633162df64152
Satyakam Mohapatra <mohapatra.satyakam91@gmail.com>
2017-08-30 18:12:24 +0530

    Added ex11.py

---
+++ ex11.py
@@ -0,0 +1,9 @@
+print("How old are you?",end=' ')
+age = input()
+print("How tall are you?",end=' ')
+height = int(input())
+height = height+10
+#3print("How much do you weight?",end =' ')
+weight = input("How much do you weight? ")
+print(f"So, you're {age} old,", (height+10), f"tall and {weight} heavy")
+print(age)
