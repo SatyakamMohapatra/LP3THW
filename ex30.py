1fe0bcfda12ead06938a16c0dfa0e8bee1727fee
Satyakam Mohapatra <mohapatra.satyakam91@gmail.com>
2017-09-04 15:39:54 +0530

    IF ELIF ELSE:

---
+++ ex30.py
@@ -0,0 +1,22 @@
+people = 30
+cars = 40
+truck = 40
+
+if cars > people:
+    print("We should take the car")
+elif cars < people:
+    print("We should not take the care.")
+else:
+    print("We can't decide")
+
+if truck > cars:
+    print("That's Too many truck")
+elif truck < cars:
+    print("Maybe we can take the Truck")
+else:
+print("We still can't decide")
+
+if people > truck:
+    print("Lets Just take the trucks.")
+else:
+    print("Fine,Lets Just stay in the home then")
